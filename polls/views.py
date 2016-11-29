8c4c5a06f3f015f72431eb7fecae87ef5db7e264
mbrunson <Techintern@kippnyc.org>
2016-11-29 00:49:16 -0500

    homework 6. 11-30-2016

--- polls/views.py
+++ polls/views.py
@@ -3,56 +3,62 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Choice, Question
 
 # Create your views here.
-class IndexView(generic.ListView):
+class IndexView(generic.DetailView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
     def get_queryset(self):
-        """Return the last five published question."""
-        return Question.Objects.order_by('-pub_date')[:5]
+        """Return the last five published question (not including those set to be published in the future)."""
+        ##return Question.Objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 #def index(request):
-#	latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#	context = { 'latest_question_list': latest_question_list}
-#	return render(request, 'polls/index.html', context)
-#	#-second output option-
-#	#template = loader.get_template('polls/index.html')
-#	#return HttpResponse(template.render(context, request))
-#	#-first output option-
-#	#output = ', '.join([q.question_text for q in latest_question_list])
-#	#return HttpResponse(output)
+#   latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#   context = { 'latest_question_list': latest_question_list}
+#   return render(request, 'polls/index.html', context)
+#   #-second output option-
+#   #template = loader.get_template('polls/index.html')
+#   #return HttpResponse(template.render(context, request))
+#   #-first output option-
+#   #output = ', '.join([q.question_text for q in latest_question_list])
+#   #return HttpResponse(output)
 
 #def detail(request, question_id):
-	#404 error shortcut
-#	question = get_objecst_or_404(Question, pk=question_id)
-#	return render(request, 'polls/details.html', {'question': question})
-	#-without the 404 import, this is what you do
-	#try:
-	#	question = Question.objects.get(pk=question_id)
-	#except Question.DoesNotExist:
-	#	raise Http404("Question does not exist")
-	#return render(request, 'polls/details.html', {'question': question})
-	#response = "you're looking at the results of the question %s."
-	#return HttpResponse(response % question_id)
+    #404 error shortcut
+#   question = get_objecst_or_404(Question, pk=question_id)
+#   return render(request, 'polls/details.html', {'question': question})
+    #-without the 404 import, this is what you do
+    #try:
+    #   question = Question.objects.get(pk=question_id)
+    #except Question.DoesNotExist:
+    #   raise Http404("Question does not exist")
+    #return render(request, 'polls/details.html', {'question': question})
+    #response = "you're looking at the results of the question %s."
+    #return HttpResponse(response % question_id)
 
 #def results(request, question_id):
-#	question = get_object_or_404(Question, pk=question_id)
+#   question = get_object_or_404(Question, pk=question_id)
  #   return render(request, 'polls/results.html', {'question':question})
     #response = "you're looking at the results of question %s."
-	#return HttpResponse(response % question_id)
+    #return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-	question = get_object_or_404(Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
